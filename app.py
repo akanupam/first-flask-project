65abe778f5e629c0c39bb966429d774f91553bec
Aditya Kumar Anupam <aditanupam@gmail.com>
2025-01-05 19:29:31 +0530

    commit modified app and templates

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS =[
+    {
+        'id': 1,
+        'service':'ML engineering',
+        'location':'Remote'
+    },
+    {
+        'id': 2,
+        'service': 'Software Development',
+        'location': 'Remote'
+    },
+    {
+        'id': 3,
+        'service': 'WEB DEV',
+        'location': 'Remote'
+    }
+]
+
 @app.route("/")
-def hello_world():
-    return "Hello world"
+def hello():
+    return render_template("home.html", jobs=JOBS)
+
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True) #to run the code
-
 
